0f03efd95d8aa2e7c1e3ed5f5461ba473a7b87c1
sonerkuzucu <31271377+sonerkuzucu@users.noreply.github.com>
2021-03-11 14:58:56 +0300

    Update HW1.py

--- Homeworks/HW1.py
+++ Homeworks/HW1.py
@@ -1,5 +1,17 @@
 #Explain your work
 
 #Question 1
-for x in range(a):
-	print(a)
+List1 = list(range(2, 11, 2))
+print (List1)
+List2 = list(range(1, 10, 2))
+print (List2)
+List = List1 + List2
+print (List)
+List.sort()
+print (List)
+
+Listx2 = [a*2 for a in List]
+print (Listx2)
+
+for x in range(len(Listx2)):
+  print (List[x], type(Listx2[x]))

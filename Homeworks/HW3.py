81623584a6f847a701e0ab68e819d1d4d6b9d7f5
sonerkuzucu <31271377+sonerkuzucu@users.noreply.github.com>
2021-03-15 09:44:23 +0300

    Update HW3.py

--- Homeworks/HW3.py
+++ Homeworks/HW3.py
@@ -1,5 +1,28 @@
-#Explain your work
+n = int(input("Kaç Öğrenci Girmek istiyorsunuz :"))
+i = 0
+passingGrade = 0.0
+Notlar ={}
+while i < n:
+  i += 1
+  adi = str(input("Adı Soyadı : "))
+  midterm = float(input("Vize Notu : "))
+  project = float(input("Proje Notu : "))
+  final = float(input("Final Notu : "))
+  passingGrade = midterm*0.3 + project*0.3 + final*0.4
+  Not = [midterm, project, final, passingGrade]
+  Notlar[adi] = Not
+isimler = {}
+for x, y in Notlar.items():
+    isimler[x] = y[-1]
 
-#Question 1
-for x in range(a):
-	print(a)
+grades = []
+for g in isimler.values():
+    grades.append(g)
+grades.sort(reverse=True)
+for r, t in isimler.items():
+    print(r.ljust(22, ' '), ": ", "{0:.1f}".format(t))
+
+print("")
+print ("Mezuniyet Notları Sıralaması")
+for g in grades:
+    print("{0:.1f}".format(g))
